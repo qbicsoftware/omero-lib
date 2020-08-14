8a7be38eb0fb7e098127108bac296283d90f12aa
Tobias Koch <KochTobi@users.noreply.github.com>
2020-08-14 10:56:17 +0200

    Append session ID to avoid manual login (#8)
    * Append session ID to avoid manual login
    
    * Use http for image download
    
    * Add spock framework
    
    * Fix Test class definition
    
    * Merge spock test classes
    
    * Rename local variable in getImageDownloadLink(long)
    
    Co-authored-by: Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>

---
+++ src/test/java/life/qbic/omero/BasicOMEROClientSpec.groovy
@@ -0,0 +1,16 @@
+package life.qbic.omero
+
+import spock.lang.Specification
+
+/**
+ * <class short description - 1 Line!>
+ *
+ * <More detailed description - When to use, what it solves, etc.>
+ *
+ * @author Sven Fillinger
+ * @since <versiontag>
+ */
+class BasicOMEROClientSpec extends Specification {
+  def "GetImageDownloadLink"() {
+  }
+}

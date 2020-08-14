0ca6d153a61057f902b90495e46f3b52efbc8181
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-08-14 10:20:15 +0200

    Fix Test class definition

---
+++ src/test/java/life/qbic/omero/BasicOMEROClientSpec.groovy
@@ -0,0 +1,13 @@
+package life.qbic.omero
+
+import spock.lang.Specification
+
+/**
+ * <short description>
+ *
+ * <detailed description>
+ *
+ * @since: <versiontag>
+ */
+class BasicOMEROClientSpec extends Specification {
+}
